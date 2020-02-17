9c788d223262fe660c037e138ec09c6e021abdad
Administrator <Administrator@SC-201903061746>
2020-02-17 18:33:33 +0800

    完成项目搭建和客户地址的联动

---
+++ src/main/java/com/situ/scrm/address/dao/TheAddressDao.java
@@ -0,0 +1,25 @@
+package com.situ.scrm.address.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.situ.scrm.address.domain.TheAddress;
+
+@Repository
+public interface TheAddressDao {
+
+	/**
+	 * 搜索所有的省份
+	 * @return
+	 */
+	List<TheAddress> findAllProvince();
+
+	/**
+	 * 根据传进的Long型的数据查询省份下的地区
+	 * @param parentCode
+	 * @return
+	 */
+	List<TheAddress> findArea(Long parentCode);
+
+}
